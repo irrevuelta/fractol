9433057ca54b53cda399e7d482417a14df5ac277
irrevuelta <irene.revueltagomez@gmail.com>
2024-11-16 14:22:31 +0100

    Update fractal.c

--- fractal.c
+++ fractal.c
@@ -3,50 +3,87 @@
 void	my_pixel_put(int x, int y, t_img *img, int color)
 {
 	*(unsigned int *)(img->img_data + (y * img->size_line + x
-						* (img->bits_per_pixel / 8))) = color;
+				* (img->bits_per_pixel / 8))) = color;
 }
 
-void	draw_mandelbrot(t_mlx_data *data)
+void	mandelbrot(t_mlx_data *data, int x, int y)
 {
 	t_complex	c;
 	t_complex	z;
 	int			iter;
 	int			color;
-    int x;
-    int y;
 
-    x = 0;
-	data->img.img_ptr = mlx_new_image(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
-	data->img.img_data = mlx_get_data_addr(data->img.img_ptr, &data->img.bits_per_pixel,
-			&data->img.size_line, &data->img.endian);
-	while(x < WINDOW_WIDTH)
+	c.real = (linear_interpol(x, -2, 2, 0, WINDOW_WIDTH) + data->move_x)
+		* data->zoom; // Pensar como implementar que me haga el movimineto proporcional y el zoom siga al raton
+	c.imag = (linear_interpol(y, 2, -2, 0, WINDOW_HEIGHT) + data->move_y)
+		* data->zoom;
+	z.real = 0;
+	z.imag = 0;
+	iter = 0;
+	while (iter < MAX_ITER)
 	{
-        y = 0;
-		
-		while(y < WINDOW_HEIGHT)
+		z = complex_add(complex_square(z), c);
+		if (module(z) > 2.0)
 		{
-			c.real = linear_interpol(x, -2, +2, 0, WINDOW_WIDTH)*data->zoom;
-			c.imag = linear_interpol(y, -2, +2, 0, WINDOW_HEIGHT)*data->zoom;
-			z.real = 0;
-			z.imag = 0;
-			iter = 0;
-			while (iter < MAX_ITER)
-			{
-				z = complex_add(complex_square(z), c);
-				if (module(z) > 2.0)
-				{
-					color = linear_interpol(iter, BLACK, WHITE, 0, MAX_ITER);
-					my_pixel_put(x, y, &data->img, color);
-					break ;
-				}
-				iter++;
-			}
-			//my_pixel_put(x, y, &data->img, WHITE); //-> ponerlo en una función aparteeee todo lo del bucle y salir con return
-            y++;
+			color = linear_interpol(iter, BLACK, WHITE, 0, MAX_ITER);
+			my_pixel_put(x, y, &data->img, color);
+			break ;
 		}
-        x++;
+		iter++;
+	}
+	if (iter == MAX_ITER)
+		my_pixel_put(x, y, &data->img, BLACK);
+}
+void	julia(t_mlx_data *data, int x, int y)
+{
+	t_complex	c;
+	t_complex	z;
+	int			iter;
+	int			color;
+
+	c.real = -0.70176;
+	c.imag = 0.3842;
+	z.real = (linear_interpol(x, 0, WINDOW_WIDTH, -2, 2) + data->move_x)
+		* data->zoom;
+	z.imag = (linear_interpol(y, 0, WINDOW_HEIGHT, 2, -2) + data->move_y)
+		* data->zoom;
+	iter = 0;
+	while (iter < MAX_ITER)
+	{
+		z = complex_add(complex_square(z), c);
+		if (module(z) > 2.0)
+		{
+			color = linear_interpol(iter, BLACK, WHITE, 0, MAX_ITER);
+			my_pixel_put(x, y, &data->img, color);
+			break ;
+		}
+		iter++;
+	}
+	if (iter == MAX_ITER)
+		my_pixel_put(x, y, &data->img, BLACK);
+}
+void	draw_fractal(t_mlx_data *data)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < WINDOW_WIDTH)
+	{
+		y = 0;
+		while (y < WINDOW_HEIGHT)
+		{
+			if (data->name[0] == '1')
+				mandelbrot(data, x, y);
+			else if (data->name[0] == '2')
+			{
+				julia(data, x, y);
+			}
+			y++;
+		}
+		x++;
 	}
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.img_ptr, 0,
 		0);
-	mlx_destroy_image(data->mlx_ptr, data->img.img_ptr);
+	// mlx_destroy_image(data->mlx_ptr, data->img.img_ptr);
 }
